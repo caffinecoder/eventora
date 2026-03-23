c5e797de62467652bd5f2ee8b606636749faf3b0
caffinecoder <sharmistajanapati.20@gmail.com>
2026-03-23 10:58:20 +0530

    d

--- eventora_backend/apps/events/admin.py
+++ eventora_backend/apps/events/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Event, Category
 
-admin.site.register(Category)
-admin.site.register(Event)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
