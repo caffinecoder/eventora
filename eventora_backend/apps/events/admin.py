5813ede88d95c2314fb155dcf9a1099728576c54
caffinecoder <sharmistajanapati.20@gmail.com>
2026-03-23 11:07:52 +0530

    d

--- eventora_backend/apps/events/admin.py
+++ eventora_backend/apps/events/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.text import slugify
 from .models import Event, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.slug:
+            obj.slug = slugify(obj.name)
+        super().save_model(request, obj, form, change)
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
