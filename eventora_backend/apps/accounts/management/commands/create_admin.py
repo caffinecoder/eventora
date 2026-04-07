cfdf52ee5e70f33fb09ad7eb30aa80c274fd58a0
caffinecoder <sharmistajanapati.20@gmail.com>
2026-04-07 07:35:53 +0530

    changes

--- eventora_backend/apps/accounts/management/commands/create_admin.py
+++ eventora_backend/apps/accounts/management/commands/create_admin.py
@@ -1,15 +1,26 @@
 from django.core.management.base import BaseCommand
 from apps.accounts.models import User
 
+
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(email='admin@eventora.com').exists():
-            User.objects.create_superuser(
-                email='admin@eventora.com',
-                password='yourpassword',
-                full_name='Admin',
-                role='admin'
-            )
-            self.stdout.write('Admin created')
-        else:
-            self.stdout.write('Admin already exists')
+        email    = 'admin@eventora.com'
+        password = 'Admin1234'
+
+        if User.objects.filter(email=email).exists():
+            User.objects.filter(email=email).delete()
+            self.stdout.write('Deleted existing admin.')
+
+        User.objects.create_superuser(
+            email      = email,
+            password   = password,
+            full_name  = 'Super Admin',
+            role       = 'admin',
+            admin_role = 'Super Admin',
+            is_active  = True,
+            is_staff   = True,
+        )
+
+        self.stdout.write(self.style.SUCCESS(
+            f'Superuser created: {email} / {password}'
+        ))
